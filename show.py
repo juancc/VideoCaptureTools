1a3eef08f10551ae20679de8c2b10f3c4a25347d
JCA <juanarbelaez@vaico.com.co>
2022-10-28 08:43:13 -0500

    Save image from cameras

--- show.py
+++ show.py
@@ -1,9 +1,10 @@
 """
-Script for show multiple the image from multiple cameras
-Using OpenCV
-
+Show multiple the cameras and store frames
 JCA
 """
+import os
+from datetime import datetime  
+
 import numpy as np
 import cv2 as cv
 
@@ -17,8 +18,11 @@ def show_cap(cap, window, scale=1, rect=True, color=(0, 0, 255)):
     """Show video capture in window"""
     # Capture frame-by-frame
     ret, frame = cap.read()
+    # if frame is read correctly ret is True
+    if not ret:
+        print("Can't receive frame (stream end?). Exiting ...")
+        return
     frame = cv.resize(frame, (0, 0), fx=scale, fy=scale)
-
     # draw square centered on frame
     if rect:
         h, w = frame.shape[:-1]
@@ -28,28 +32,29 @@ def show_cap(cap, window, scale=1, rect=True, color=(0, 0, 255)):
         # represents the bottom right corner of rectangle
         end_point = (w//2+halfsize, h//2+halfsize)
         
-        # Line thickness of 2 px
         thickness = 1
-        
-        # Using cv2.rectangle() method
-        # Draw a rectangle with blue line borders of thickness of 2 px
-        image = cv.rectangle(frame, start_point, end_point, color, thickness)
+        cv.rectangle(frame, start_point, end_point, color, thickness)
 
-    # if frame is read correctly ret is True
-    if not ret:
-        print("Can't receive frame (stream end?). Exiting ...")
-        return
-    # Our operations on the frame come here
-    # gray = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
-    # Display the resulting frame
     cv.imshow(window, frame)
 
-def save_im(caps):
-    """Save frame capture"""
 
+def save_im(cap, idx, savepath):
+    """Save frame capture"""
+    ret, frame = cap.read()
+    if ret:
+        now = datetime.now()
+        timestamp = now.strftime("%d-%b-%Y-%H|%M|%S") + f'_{idx}'
+        filepath = os.path.join(savepath, f'{timestamp}.png')
+        cv.imwrite(filepath, frame)
+        print(f'Saved image from camera {idx}')
+    else:
+        print("Could't save frame!º")
 
 
 def main():
+    print(f'Creating directory: {SAVE_PATH}')
+    os.makedirs(SAVE_PATH, exist_ok=True)
+
     print('Loading cameras...')
     caps = [cv.VideoCapture(i) for i in range(CAM_NUM) if cv.VideoCapture(i).isOpened]
     
@@ -63,8 +68,12 @@ def main():
         key = cv.waitKey(1)
         if key == ord('q'):
             break
-        if key == ord('a'):
+        if key == ord('s'):
             print('Saving photos')
+            i=0
+            for cap in caps:
+                save_im(cap, i, SAVE_PATH)
+                i+=1
             # cv.imwrite('foto.jpg', frame)
     # When everything done, release the capture
     cap.release()
