c5fdd8fa895bdc6778d7581c4c02afce3f936773
Juanc <jkaz.os@gmail.com>
2023-01-16 13:56:42 -0500

    added arg param

--- Tools/show_cam.py
+++ Tools/show_cam.py
@@ -4,6 +4,7 @@ JCA
 """
 import os
 from datetime import datetime  
+import argparse
 
 import numpy as np
 import cv2 as cv
@@ -12,6 +13,14 @@ import cv2 as cv
 CAM_NUM = 3
 SCALE = 0.5
 SAVE_PATH = 'Captures'
+
+parser = argparse.ArgumentParser(
+                    prog = 'Camera Show',
+                    description = 'Show videos from multiple cameras',
+                    epilog = 'Press Q to end recording. Press S to save frames')
+
+parser.add_argument('cam', help='Number of cameras', type=int, default=1)
+parser.add_argument('-s', '--scale', help='Scale frames to show', default=SCALE, type=float)
 
 
 def show_cap(cap, window, scale=1, rect=True, color=(0, 0, 255)):
@@ -51,18 +60,19 @@ def save_im(cap, idx, savepath):
         print("Could't save frame!º")
 
 
-def main():
+def main(cam_num, scale):
+
     print(f'Creating directory: {SAVE_PATH}')
     os.makedirs(SAVE_PATH, exist_ok=True)
 
     print('Loading cameras...')
-    caps = [cv.VideoCapture(i) for i in range(CAM_NUM) if cv.VideoCapture(i).isOpened]
+    caps = [cv.VideoCapture(i) for i in range(cam_num) if cv.VideoCapture(i).isOpened]
     
     print('Starting')
     while True:
         i=0
         for cap in caps:
-            show_cap(cap, f'cam-{i}', scale=SCALE)
+            show_cap(cap, f'cam-{i}', scale=scale)
             i+=1
 
         key = cv.waitKey(1)
@@ -80,4 +90,7 @@ def main():
     cv.destroyAllWindows()
 
 if __name__ == '__main__':
-    main()
+    args = parser.parse_args()
+    main(args.cam, args.scale)
+
+
